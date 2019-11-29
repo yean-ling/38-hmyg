05fb7aebc55355a3fcee696bc33a0e55d8eaccda
yean-ling <2509758936@qqcom>
2019-11-29 18:59:53 +0800

    首页异步请求的封装

---
+++ request/request.js
@@ -0,0 +1,16 @@
+function axios({url, ...params}) {
+    const baseURL="https://api.zbztb.cn/api/public/v1/";
+    return new Promise((resolve,reject) =>{
+        wx.request({
+            url:baseURL + url,
+            ...params,
+            success: (result) =>{
+                resolve(result);
+            }
+        });
+    })
+    
+}
+
+
+export default axios;
