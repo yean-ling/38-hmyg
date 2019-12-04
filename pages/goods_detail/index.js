33167a782918e2be144002ba3fd14f23761c4b04
yean-ling <2509758936@qqcom>
2019-12-04 19:14:06 +0800

    商品的详情页面和静态页面完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,23 +1,34 @@
-// pages/goods_detail/index.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+/*
+1 发送请求获取数据 页面渲染
 
+*/
+
+
+import request from "../../request/request";
+
+
+Page({
+  data:{
+      goodsInfo:{}
+  },
+  onLoad:function(options){
+     this.getDetail(options.goods_id);       
+           
   },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  // 获取商品的详情数据
+  getDetail(goods_id) {
+    request({
+      url:"goods/detail",
+      data:{
+        goods_id
+      }
+    }).then(res=>{
+      console.log(res);
+        this.setData({
+          goodsInfo:res.data.message
+        })
+      
+    })
   }
 })
